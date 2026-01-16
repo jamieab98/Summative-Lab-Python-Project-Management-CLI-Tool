da2a6c51da3703b685e5db21330930e9ea4f1187
Jamie Bryson <jamieab98@gmail.com>
2026-01-16 17:14:02 -0500

    Created the add_project subcommand

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ subparsers = parser.add_subparsers(dest = "command", required=True)
 add_user = subparsers.add_parser("add_user")
 add_user.add_argument("user", type=str)
 
+add_project = subparsers.add_parser("add_project")
+add_project.add_argument("project", type=str)
+
 args = parser.parse_args()
 
 if args.command == "add_user":
     print(args.user)
+if args.command == "add_project":
+    print(args.project)
