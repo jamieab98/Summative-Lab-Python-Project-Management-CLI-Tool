d7f2d4ea619f2ccdbe5fa3abf2188f80327e960b
Jamie Bryson <jamieab98@gmail.com>
2026-01-16 17:11:14 -0500

    Created the add_user subcommand

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ import argparse
 parser = argparse.ArgumentParser(description = "Program for managers to manage their users, projects, and tasks")
 subparsers = parser.add_subparsers(dest = "command", required=True)
 
+add_user = subparsers.add_parser("add_user")
+add_user.add_argument("user", type=str)
+
 args = parser.parse_args()
+
+if args.command == "add_user":
+    print(args.user)
