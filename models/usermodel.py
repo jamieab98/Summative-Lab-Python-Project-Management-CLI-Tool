72c69c05726ab76d12db548d448bedb9e57a9fce
Jamie Bryson <jamieab98@gmail.com>
2026-01-16 17:58:46 -0500

    Modified usermodel to add the new user to the users list and return self.name when the instance is called

--- models/usermodel.py
+++ models/usermodel.py
@@ -1,6 +1,11 @@
 class User:
     users = []
+
     def __init__ (self, name, email):
         self.name = name
         self.email = email
         self.projects = []
+        User.users.append(self)
+
+    def __repr__(self):
+        return(self.name)
