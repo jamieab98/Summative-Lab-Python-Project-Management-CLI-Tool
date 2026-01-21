774689aabe75280c180277108aa8ed00fce1bcf7
Jamie Bryson <jamieab98@gmail.com>
2026-01-21 17:00:36 -0500

    Added the @property method for task.id and task.title

--- models/taskmodel.py
+++ models/taskmodel.py
@@ -2,10 +2,18 @@ class Task:
     tasks = []
 
     def __init__(self, title, status="incomplete"):
-        self.title = title
+        self._title = title
         self.status = status
-        self.id = len(Task.tasks)+1
+        self._id = len(Task.tasks)+1
         Task.tasks.append(self)
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def title(self):
+        return self._title
     
     '''def __repr__(self):
         return self.status'''
